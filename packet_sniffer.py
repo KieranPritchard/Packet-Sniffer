39c80685f152cef70c45de5158bb1aa76dcd0c9e
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-08 16:12:27 +0000

    added comments

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,19 +1,28 @@
-from scapy.all import *
+from scapy.all import Ether, IP, TCP, sniff
 
 # Function to output packet
 def output_packet(packet):
+    # Checks if the packet has an ethernet layer
     if Ether in packet:
+        # Extracts the ethernet layer
         ethernet_layer = packet[Ether]
+        # Outputs the ethernet source and destination address
         print(f"Ether Source: {ethernet_layer.src}")
         print(f"Ether Destination: {ethernet_layer.dst}")
 
+    # Checsk if the packet has an ip layer
     if IP in packet:
+        # Extracts the ip layer
         ip = packet[IP]
+        # Outputs the ip source and destination address
         print(f"IP Source {ip.src}")
         print(f"IP Destination {ip.dst}")
 
+    # Checks for the tcp layer i the packet
     if TCP in packet:
+        # Extracts the ip layer
         tcp_layer = packet[TCP]
+        # Outputs the ip source and destination address
         print(f"Source Port: {tcp_layer.sport}")
         print(f"Destination Port: {tcp_layer.dport}")
 
