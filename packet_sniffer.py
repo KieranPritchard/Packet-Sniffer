78a7988c660677c74499aaa6d0e9485443362e88
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-08 14:15:06 +0000

    added basic packet sniffer features

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,1 +1,20 @@
 from scapy.all import *
+
+# Function to output packet
+def output_packet(packet):
+    if Ether in packet:
+        ethernet_layer = packet[Ether]
+        print(f"Source destination: {ethernet_layer.src}")
+        print(f"Destination Port: {ethernet_layer.dst}")
+
+    if IP in packet:
+        ip = packet[IP]
+        print(f"IP  {ip.src} → {ip.dst}")
+
+    if TCP in packet:
+        tcp_layer = packet[TCP]
+        print(f"Source Port: {tcp_layer.sport}")
+        print(f"Destination Port: {tcp_layer.dport}")
+
+# Sniff packets on the 'eth0' interface
+sniff(iface='en0', prn=output_packet)
