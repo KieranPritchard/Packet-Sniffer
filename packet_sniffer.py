b54dd7ba20a205c068360011fba62f389bb9da1a
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-08 14:27:23 +0000

    added ethernet and ip checking to sniffer

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -4,17 +4,18 @@ from scapy.all import *
 def output_packet(packet):
     if Ether in packet:
         ethernet_layer = packet[Ether]
-        print(f"Source destination: {ethernet_layer.src}")
-        print(f"Destination Port: {ethernet_layer.dst}")
+        print(f"Ether Source: {ethernet_layer.src}")
+        print(f"Ether Destination: {ethernet_layer.dst}")
 
     if IP in packet:
         ip = packet[IP]
-        print(f"IP  {ip.src} → {ip.dst}")
+        print(f"IP Source {ip.src}")
+        print(f"IP Destination {ip.dst}")
 
     if TCP in packet:
         tcp_layer = packet[TCP]
         print(f"Source Port: {tcp_layer.sport}")
         print(f"Destination Port: {tcp_layer.dport}")
 
-# Sniff packets on the 'eth0' interface
+# Sniff packets on the 'en0' interface
 sniff(iface='en0', prn=output_packet)
